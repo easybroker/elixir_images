fac3ffe503615f346cb96fd0f4077aa279d0eedd
David Padilla <david@easybroker.com>
2015-09-16 23:33:23 -0500

    Now we print filenames

--- lib/images.ex
+++ lib/images.ex
@@ -3,10 +3,15 @@ defmodule Images do
     import Supervisor.Spec
 
     children = [
-      worker(Images.Repo, [])
+      worker(Images.Repo, []),
+      worker(Task, [fn -> Images.run end])
     ]
 
     opts = [strategy: :one_for_one, name: Images.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def run do
+    Enum.each Images.PropertyImage.all, fn(i) -> IO.puts i.file end
   end
 end
